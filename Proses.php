76945170f499e029342f6bace64bcb0d2854f5c5
udin <awaludinnurmuhammad201@gmail.com>
2019-10-14 14:12:31 +0700

    first commit

---
+++ Proses.php
@@ -0,0 +1,9 @@
+<!-- pembuat kode php -->
+<?php
+// mengambil data dari yang dikirim
+echo $_GET['nama'];
+// memberi break atau enter
+echo "<br />";
+// mengambi data dari url
+echo $_GET['email'];
+?>
